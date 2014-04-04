53a02f2159be1ae5e9af2d10a6066e828197db02
Riley Parsons <macowner@Riley-Parsons-MacBook-Pro.local>
2014-04-03 20:55:38 -0700

    Initial Commit

---
+++ SceneScope/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SceneScope
+//
+//  Created by Riley Parsons on 4/3/14.
+//  Copyright (c) 2014 Riley Parsons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SceneScope/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SceneScope
+//
+//  Created by Riley Parsons on 4/3/14.
+//  Copyright (c) 2014 Riley Parsons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
